1b710906d5bcc1627e088e6008f02435f07b36be
Kevin O'Connor <96597458+k-oconnor@users.noreply.github.com>
2025-11-25 08:26:33 -0600

    Delete kevin-site directory

--- kevin-site/_projects/project3.md
+++
@@ -1,6 +0,0 @@
----
-title: "Project 1"
-date: 2025-08-01
-layout: page
----
-Description of Project 1. [Link to demo/repo](#).
--- kevin-site/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.brain-svg a').forEach(link => {
-  link.addEventListener('mouseover', () => link.querySelector('text').style.fill = '#ff00ff');
-  link.addEventListener('mouseout', () => link.querySelector('text').style.fill = '#fff');
-});
